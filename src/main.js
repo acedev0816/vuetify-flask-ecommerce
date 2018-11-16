618feb0e97d80ee37d1b349ac4cdfd74b39fd0f2
acedev0816 <acedev0816@gmail.com>
2018-11-15 20:52:13 -0800

    HelloWorld.vue -> VueInfo.vue; Add Vuetify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 
 import './filters'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <VueInfo msg="Welcome to the Vue.js + Vuetify + Flask + Vega App Example"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueInfo from '@/components/VueInfo.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    VueInfo
   }
 }
 </script>
